6b995488e1513b28f21d610849a195e62d483456
AK01101001 <147595111+AK01101001@users.noreply.github.com>
2025-02-12 15:25:40 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace galeria
+{
+    public partial class MainWindow : Window
+    {
+        public List<zdjecie> zdjecia { get; set; } = new List<zdjecie>();
+        string path = "../../../Data.txt";
+        int index = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            otworz();
+            this.Closing += OnClose;
+        }
+
+        private void OnClose(object? sender, CancelEventArgs e)
+        {
+            zapisz();
+        }
+
+        private void otworz()
+        {
+            string[] dane = File.ReadAllLines(path);
+            for (int i = 0; i < dane.Count(); i+=2)
+            {
+                zdjecia.Add(new zdjecie(int.Parse(dane[i]), int.Parse(dane[i + 1])));
+            }
+            wyswietl();
+        }
+        private void zapisz()
+        {
+            List<string> dane = new List<string>();            
+            for (int i = 0; i < zdjecia.Count(); i++)
+            {
+                dane.Add(zdjecia.ElementAt(i).lWyswietlen.ToString());
+                dane.Add(zdjecia.ElementAt(i).lPolubien.ToString());
+            }
+            File.WriteAllLines(path,dane);
+        }
+
+        private void prawo(object sender, RoutedEventArgs e)
+        {
+            index++;
+            if (index > zdjecia.Count()-1)
+            {
+                index = 0;
+            }
+            wyswietl();
+        }
+
+        
+
+        private void lewo(object sender, RoutedEventArgs e)
+        {
+            index--;
+            if (index <0)
+            {
+                index = zdjecia.Count()-1;
+            }
+            wyswietl();
+        }
+        private void wyswietl()
+        {
+            zdjecia.ElementAt(index).lWyswietlen++;
+            obraz.Source = new BitmapImage(new Uri( zdjecia.ElementAt(index).source,UriKind.Relative));
+            wyswietlenia.Text = zdjecia.ElementAt(index).lWyswietlen.ToString();
+            polubienia.Text = zdjecia.ElementAt(index).lPolubien.ToString();
+        }
+
+        private void polub(object sender, RoutedEventArgs e)
+        {
+            zdjecia.ElementAt(index).lPolubien++;
+            polubienia.Text = zdjecia.ElementAt(index).lPolubien.ToString();
+        }
+    }
+}
---
+++ zdjecie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace galeria
+{
+    public class zdjecie
+    {
+        public string source { get; set; } = "";
+
+        public int lWyswietlen { get; set; }
+        public int lPolubien { get; set; }
+        static int licznik;
+
+        public zdjecie(int lWyswietlen, int lPolubien)
+        {
+            licznik++;
+            this.source= "zdj/zdj"+licznik+".png";
+            this.lWyswietlen = lWyswietlen;
+            this.lPolubien = lPolubien;
+        }
+    }
+}
